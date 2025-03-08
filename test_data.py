41ecbe0c1311c926ab6a65706f68d6f298cac3d9
bendreality <benjamin.juergens83@gmail.com>
2025-03-08 13:58:49 +0100

    Working tweaked. wrote external test data file for better testing

---
+++ test_data.py
@@ -0,0 +1,19 @@
+data_list_1 = [
+    [1, "Yakup Tanrikulu", "Giftforwed", "Hardenbergstraße 83, 51373 Leverkusen", ""],
+    [4, "Johannes Müller", "Craft Beer Shop", "Allensteiner Str. 15, 51373 Leverkusen", ""],
+    [2, "Jörg Müller", "Biofleischerei Müller", "Görresstraße 1, 51373 Leverkusen", ""],
+    [3, "", "Gadies Glücksmomente", "Lötzener Str. 3, 51373 Leverkusen", ""]
+]
+
+data_list_2 = [
+    [1, "TECTRION GmbH", "", "Kaiser-Wilhelm-Allee 80, 51373 Leverkusen", "930"][1],
+    [2, "", "Chemion Logistik GmbH", "Kaiser-Wilhelm-Allee 80, 51373 Leverkusen", "869"][1],
+    [3, "", "KRONOS TITAN GmbH", "Peschstraße 5, 51373 Leverkusen", "820"][1],
+    [4, "", "Wuppermann AG", "Ottostr. 5, 51381 Leverkusen", "700"][1],
+    [5, "", "Bayer Gastronomie GmbH", "Kaiser-Wilhelm-Allee 3, 51373 Leverkusen", "680"][1],
+    [6, "", "Momentive Performance Materials GmbH", "Chempark, Building V, 51368 Leverkusen", "576"][1],
+    [7, "Josef Wallraff", "Josef Wallraff GmbH & Co. KG Elektro Groß- und Einzelhandel", "Quettinger Str. 240, 51381 Leverkusen", "116"][1],
+    [8, "", "Ascensia Diabetes Care Deutschland GmbH", "Marie-Curie-Str. 5, 51377 Leverkusen", "115"][1],
+    [9, "", "RELOGA Holding GmbH & Co. KG", "Robert-Blum-Str. 8, 51373 Leverkusen", "114"][1],
+    [10, "", "Biofrontera AG", "Hemmelrather Weg 201, 51377 Leverkusen", "100"][1]
+]
